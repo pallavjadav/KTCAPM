04ed59cef728f9a2e1151d8ca976e24d5eefbbb2
Pallavkumar Jadav <pallavjadav@gmail.com>
2023-06-07 11:11:37 +0000

    KTSessionDay2

--- srv/service.js
+++ srv/service.js
@@ -2,6 +2,31 @@ const cds = require('@sap/cds')
 
 module.exports = cds.service.impl(async function () {
 
+    //this.before 
+    this.before('POST', 'CaptainCool', async function (req) {
+        var { fullname } = req.data
+        // req.data = {
+        //     "id": "4",
+        //     "fullname": "varghese",
+        //     "doj": "2008-09-01",
+        //     "age": 40
+        // }
+        req.data.fullname = fullname.toUpperCase()
+
+        // req.data = {
+        //     "id": "4",
+        //     "fullname": "VARGHESE",
+        //     "doj": "2008-09-01",
+        //     "age": 40
+        // }
+    })
+
+    this.after('READ', 'CaptainCool', each =>
+
+        each.age = each.age + 5
+
+    )
+
     this.on('POST', 'CaptainCool', async function (req) {
         console.log(req.data)
         var { id, fullname, doj, age } = req.data;
